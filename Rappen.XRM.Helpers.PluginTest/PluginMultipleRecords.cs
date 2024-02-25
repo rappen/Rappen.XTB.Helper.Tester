538d2a882f03cd365033fadf46370e8daef7634a
Jonas Rapp <jonas@jonasr.app>
2024-02-25 20:21:07 +0100

    Added Plugin Tests on Update and UpdateMultiple

---
+++ Rappen.XRM.Helpers.PluginTest/PluginMultipleRecords.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using Rappen.XRM.Helpers.Extensions;
+using Rappen.XRM.Helpers.Plugin;
+using System.Linq;
+
+namespace Rappen.XRM.Helpers.PluginTest
+{
+    public class PluginMultipleRecords : PluginBase
+    {
+        public override void Execute(PluginBag bag)
+        {
+            bag.Logger.Log($"{bag.EntityTypeCollection.Count()} records");
+            foreach (var recordtypes in bag.EntityTypeCollection)
+            {
+                var account = recordtypes.Target;
+                bag.Logger.Log($"Checking {recordtypes.Complete["name"]}");
+                var preimage = recordtypes.PreImage;
+                var newfax = account.GetAttributeValue("fax", string.Empty);
+                var prefax = preimage.GetAttributeValue("fax", string.Empty);
+                if (string.IsNullOrEmpty(newfax) != string.IsNullOrEmpty(prefax))
+                {
+                    throw new InvalidPluginExecutionException("If you have fax, you have fax. If not, you'll never get a fax.");
+                }
+            }
+        }
+    }
+}
---
+++ Rappen.XRM.Helpers.PluginTest/PluginOneRecord.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using Rappen.XRM.Helpers.Extensions;
+using Rappen.XRM.Helpers.Plugin;
+
+namespace Rappen.XRM.Helpers.PluginTest
+{
+    public class PluginOneRecord : PluginBase
+    {
+        public override void Execute(PluginBag bag)
+        {
+            var account = bag.EntityType.Target;
+            bag.Logger.Log($"Checking {bag.EntityType.Complete["name"]}");
+            var preimage = bag.EntityType.PreImage;
+            var newfax = account.GetAttributeValue("fax", string.Empty);
+            var prefax = preimage.GetAttributeValue("fax", string.Empty);
+            if (string.IsNullOrEmpty(newfax) != string.IsNullOrEmpty(prefax))
+            {
+                throw new InvalidPluginExecutionException("If you have fax, you have fax. If not, you'll never get a fax.");
+            }
+        }
+    }
+}
